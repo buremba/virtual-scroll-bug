b5e90549ab3baa69306495546db87c22f67b1837
Burak Emre Kabakcı <emrekabakci@gmail.com>
2019-02-19 20:10:20 +0300

    Initial commit

---
+++ components/Logo.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+fsdfd
+<link rel="stylesheet" href="//unpkg.com/vue-virtual-scroller@1.0.0-beta.6/dist/vue-virtual-scroller.css">
+
+  <RecycleScroller
+    style="height:100%"
+    :items="list"
+    :item-height="32">
+    <div slot-scope="{ item }" class="user">
+      {{ item }}
+    </div>
+  </RecycleScroller>
+  </div>
+</template>
+<script>
+import { RecycleScroller } from 'vue-virtual-scroller'
+export default {
+  components: {RecycleScroller},
+  data () {
+    return {
+    list: [{test: 1}, {test:3}]
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="container">
+    <link rel="stylesheet" href="//unpkg.com/vue-virtual-scroller@1.0.0-beta.6/dist/vue-virtual-scroller.css">
+
+    <div style="width:700px;height:700px;border:1px solid #ddd;margin:50px auto">
+      <RecycleScroller
+        style="height:100%"
+        :items="list"
+        :item-height="32">
+        <div slot-scope="{ item }" style="background:red;color:#fff">
+          {{ item }}
+        </div>
+      </RecycleScroller>
+    </div>
+  </section>
+</template>
+
+<script>
+  import { RecycleScroller } from 'vue-virtual-scroller'
+
+  export default {
+    components: {RecycleScroller},
+    data () {
+      return {
+        list: Array.from({length: 50}, (v, i) => { return {a: i}})
+      }
+    }
+  }
+</script>
